56ab2c0fa414ee4c54a676174186a133c2d9e145
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-07-19 23:24:40 +0300

    feat: add AboutProject component

---
+++ src/components/AboutProject/AboutProject.js
@@ -0,0 +1,28 @@
+import './AboutProject.css';
+import React from 'react';
+
+function AboutProject() {
+  return (
+    <section className="about-project">
+      <h2 className="about-project__title">О проекте</h2>
+      <ul className="about-project__cards">
+        <li className="about-project__card">
+          <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
+          <p className="about-project__description">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
+        </li>
+        <li className="about-project__card">
+          <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
+          <p className="about-project__description">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
+        </li>
+      </ul>
+      <div className="about-project__time">
+        <p className="about-project__text about-project__text_time about-project__text_time_backend">1 неделя</p>
+        <p className="about-project__text about-project__text_time about-project__text_time_frontend">4 недели</p>
+        <p className="about-project__text about-project__text_time_description">Back-end</p>
+        <p className="about-project__text about-project__text_time_description">Front-end</p>
+      </div>
+    </section>
+  )
+}
+
+export default AboutProject;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import PageNotFound from '../PageNotFound/PageNotFound.js';
 import Promo from '../Promo/Promo';
+import AboutProject from '../AboutProject/AboutProject';
 
 function App() {
   return (
     <>
       {/* <PageNotFound /> */}
-      <Promo />
+      {/* <Promo /> */}
+      <AboutProject />
     </>
   );
 }
