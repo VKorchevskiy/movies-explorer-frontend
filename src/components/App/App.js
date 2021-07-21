1ab4bb2665a92facfa3758dd4e9ccb5ba3b65749
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-07-21 16:25:03 +0300

    feat: add component Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import PageNotFound from '../PageNotFound/PageNotFound.js';
 import Landing from '../Landing/Landing';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <>
       {/* <PageNotFound /> */}
       <Landing />
+      <Footer />
     </>
   );
 }
