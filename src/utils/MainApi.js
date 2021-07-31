e986abf829560599990f8a392eb0f2b99c9d6704
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-07-31 19:03:34 +0300

    feat: добавить запросы к своему api

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,29 +1,71 @@
-import { optionsMainApi, headers } from '../../../Other/constant';
+import { optionsMainApi, headers } from './constant';
 import { checkResponse } from './halpers';
 
 class MainApi {
   constructor(config, headers) {
-    this.baseUrl = config.baseUrl;
-    this.headers = headers;
+    this._baseUrl = config.baseUrl;
+    this._headers = headers;
   }
 
-  register = ({ name, email, password }) => fetch(`${this.baseUrl}/signup`, {
-    headers: this.headers,
+  _getAuthHeaders(jwt) {
+    return {
+      ...this._headers,
+      'Authorization': `Bearer ${jwt}`,
+    }
+  }
+
+  register = ({ name, email, password }) => fetch(`${this._baseUrl}/signup`, {
+    headers: this._headers,
     method: 'POST',
     body: JSON.stringify({ name, email, password }),
   })
     .then(checkResponse)
   // .then(data => console.log(data))
 
-  authorize = ({ email, password }) => fetch(`${this.baseUrl}/signin`, {
-    headers: this.headers,
+  authorize = ({ email, password }) => fetch(`${this._baseUrl}/signin`, {
+    headers: this._headers,
     method: 'POST',
     body: JSON.stringify({ email, password }),
   })
-    .then(checkResponse);
+    .then(checkResponse)
   // .then(data => console.log(data))
 
+  getCurrentUser = (jwt) => fetch(`${this._baseUrl}/users/me`, {
+    headers: this._getAuthHeaders(jwt),
+    method: 'GET',
+  })
+    .then(checkResponse)
 
+  putchUser = ({ name, email }, jwt) => fetch(`${this._baseUrl}/users/me`, {
+    headers: this._getAuthHeaders(jwt),
+    method: 'PATCH',
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    })
+  })
+    .then(checkResponse)
+
+  getSavedMovies = (jwt) => fetch(`${this._baseUrl}/movies`, {
+    headers: this._getAuthHeaders(jwt),
+    method: 'GET',
+  })
+    .then(checkResponse)
+
+  saveMovie = (movie, jwt) => fetch(`${this._baseUrl}/movies`, {
+    headers: this._getAuthHeaders(jwt),
+    method: 'POST',
+    body: JSON.stringify({
+      ...movie,
+    })
+  })
+    .then(checkResponse)
+
+  deleteMovie = (id, jwt) => fetch(`${this._baseUrl}/movies/${id}`, {
+    headers: this._getAuthHeaders(jwt),
+    method: 'DELETE',
+  })
+    .then(checkResponse)
 
 }
 
