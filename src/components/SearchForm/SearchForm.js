66113c937cc98e0a975a3989fd5f880adbf3ca0e
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-08-05 22:15:17 +0300

    feat: Добавить валидацию формы поиска

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,37 +1,39 @@
 import './SearchForm.css';
-import React, { useState } from 'react';
-
+import React, { useState, useCallback, useEffect } from 'react';
+import { useFormWithValidation } from '../../hooks/useFormWithValidation';
 
 function SearchForm({ children, className, searchMovies }) {
+  const { values, setValues, handleChange, errors, setErrors, isValid, setIsValid, resetForm } = useFormWithValidation();
 
-  const [dataSearch, setDataSearch] = useState({
-    search: '',
-  });
+  useEffect(() => {
+    setIsValid(true)
+  }, [])
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setDataSearch({
-      ...dataSearch,
-      [name]: value,
-    });
-  };
+  useEffect(() => {
+    isValid ? setErrors({ ...errors, search: '' }) : setErrors({ ...errors, search: 'Нужно ввести ключевое слово' })
+  }, [isValid])
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!dataSearch.search) {
+
+    if (isValid.search) {
       return;
     }
-    searchMovies(dataSearch);
+    searchMovies(values);
+    resetForm();
+    setIsValid(true);
   };
 
   return (
     <div className={`search ${className || ''}`.trim()}>
       <form className="search__form" onSubmit={handleSubmit} >
         <input className="search__input-text" type="text" name="search" id="search"
-          placeholder="Фильм" required value={dataSearch.search} onChange={handleChange} />
-        <input className="button search__submit-button" type="submit" name="search" value="" />
+          placeholder="Фильм" required value={values.search || ''} onChange={handleChange}  />
+
+        <input className={`button search__submit-button ${isValid ? '':'search__submit-button_disable'}`.trim()} type="submit" name="search" value="" disabled={!isValid} />
       </form>
       {children}
+      <span className="search__error-text">{errors.search}</span>
     </div>
   )
 }
